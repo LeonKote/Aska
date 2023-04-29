847185ebb95e99c56b58079ae081e675cdc4a4cf
unknown <truefoldergames@gmail.com>
2023-04-29 06:01:42 +0500

    quiz editor

--- Assets/Scripts/Quiz/QuizQuestion.cs
+++ Assets/Scripts/Quiz/QuizQuestion.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+
+[System.Serializable]
 public class QuizQuestion
 {
 	public string question;
@@ -5,4 +8,15 @@ public class QuizQuestion
 	public string image;
 	public int time;
 	public int countdown;
+	public int rightAnswerIndex;
+
+	public QuizQuestion(string question, string[] answers, string image, int time, int countdown, int rightAnswerIndex)
+	{
+		this.question = question;
+		this.answers = answers;
+		this.image = image;
+		this.time = time;
+		this.countdown = countdown;
+		this.rightAnswerIndex = rightAnswerIndex;
+	}
 }
