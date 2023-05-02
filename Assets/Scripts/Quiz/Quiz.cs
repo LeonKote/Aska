50d0b7f9f924456891a2ba8e14bcac30cb71dfed
LeonKote <roman892265@gmail.com>
2023-05-02 13:43:52 +0500

    quiz editor (truefolder)

--- Assets/Scripts/Quiz/Quiz.cs
+++ Assets/Scripts/Quiz/Quiz.cs
@@ -1,5 +1,19 @@
+using System.Collections.Generic;
+
+[System.Serializable]
 public class Quiz
 {
 	public string id;
 	public string name;
+	public string image;
+	public string description;
+	public List<QuizQuestion> questions;
+
+	public Quiz(string name, string description, string image, List<QuizQuestion> questions)
+	{
+		this.name = name;
+		this.description = description;
+		this.image = image;
+		this.questions = questions;
+	}
 }
